b92ef8b4f3aab77ab7a813fbbd11e364b63aa675
Gerald <gerald.gsellmann@edu.fh-joanneum.at>
2019-04-07 13:37:13 +0200

    added new project including Main.java, CalculatorImpl.java, Calculator.java and pom.xml

---
+++ src/main/java/CalculatorImpl.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class CalculatorImpl implements Calculator{
+
+    private ArrayList<Integer> valueArray = new ArrayList();
+    private int temp = 0;
+
+    public int sum(){
+        return 0;
+    }
+
+    public int getMaximum(){
+        return 0;
+    }
+
+    public int getMinimum(){
+        return 0;
+    }
+
+    public void addValue(int value){
+        valueArray.add(value);
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// TODO implement your own Calculator Implementation and create a new CalculatorImpl Class
+		Calculator calc = new CalculatorImpl();
+		
+		calc.addValue(20);	// currently highest number
+		calc.addValue(4);	// currently lowest number
+		calc.addValue(16);
+		
+		// add other integers
+		
+		// output lowest number 
+		
+		// output highest number
+		
+		// output sum of all numbers
+
+	}
+
+}
